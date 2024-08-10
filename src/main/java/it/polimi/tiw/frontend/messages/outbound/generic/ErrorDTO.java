7cd22d49526ad5e0a86c3592d8e2240a4c0578b6
lorenzo-cmyk <10797603@polimi.it>
2024-08-10 23:55:50 +0200

    [messages] Implemented ErrorDTO.java

---
+++ src/main/java/it/polimi/tiw/frontend/messages/outbound/generic/ErrorDTO.java
@@ -0,0 +1,27 @@
+package it.polimi.tiw.frontend.messages.outbound.generic;
+
+/**
+ * Data Transfer Object (DTO) for error messages.
+ * This class is used to encapsulate error messages to be sent in responses.
+ */
+public class ErrorDTO {
+    private final String error;
+
+    /**
+     * Constructs a new ErrorDTO with the specified error message.
+     *
+     * @param error the error message
+     */
+    public ErrorDTO(String error) {
+        this.error = error;
+    }
+
+    /**
+     * Gets the error message.
+     *
+     * @return the error message
+     */
+    public String getError() {
+        return error;
+    }
+}
