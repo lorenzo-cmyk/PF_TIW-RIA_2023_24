6d674ea0a0060d1645cd786b4d8313f4ba394f24
lorenzo-cmyk <10797603@polimi.it>
2024-08-10 23:56:54 +0200

    [frontend] Implemented /api/logout as REST endpoint.

---
+++ src/main/java/it/polimi/tiw/frontend/controllers/authentication/UserDisconnectionServlet.java
@@ -0,0 +1,30 @@
+package it.polimi.tiw.frontend.controllers.authentication;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * This servlet is used to handle the disconnection of a user.
+ */
+@WebServlet(name = "UserDisconnectionServlet", value = "/api/logout")
+public class UserDisconnectionServlet extends HttpServlet {
+    /**
+     * Default constructor, called by the servlet container.
+     */
+    public UserDisconnectionServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        // Invalidate the session
+        req.getSession().invalidate();
+        // Reply with a 200 OK status
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        doGet(req, resp);
+    }
+}
