ef607ad821f2c040710c6d5960a8ef6cd257bea5
lorenzo-cmyk <10797603@polimi.it>
2024-09-02 15:37:20 +0200

    [filters] Revisited filters chain.

--- src/main/java/it/polimi/tiw/frontend/filters/FilterBadRequests.java
+++ src/main/java/it/polimi/tiw/frontend/filters/FilterBadRequests.java
@@ -1,7 +1,5 @@
 package it.polimi.tiw.frontend.filters;
 
-import com.google.gson.Gson;
-import it.polimi.tiw.frontend.messages.outbound.generic.ErrorDTO;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -23,11 +21,11 @@ public class FilterBadRequests implements Filter {
      * @param servletRequest  the ServletRequest object
      * @param servletResponse the ServletResponse object
      * @param filterChain     the FilterChain object
-     * @throws ServletException if a servlet-specific error occurs
      * @throws IOException      if an I/O error occurs
+     * @throws ServletException if a servlet-specific error occurs
      */
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws ServletException, IOException {
+            throws IOException, ServletException {
         // Force the servlet container to treat the request and response as HTTP
         HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
         HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
@@ -36,50 +34,40 @@ public class FilterBadRequests implements Filter {
         String requestedURL = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
         String HTTPMethod = httpRequest.getMethod();
 
-        // Retrieve the endpoint enumeration corresponding to the requested URL
-        EndpointEnumeration retrievedEndpoint = EndpointEnumeration.retrieveEndpoint(requestedURL, HTTPMethod);
-
-        // If the endpoint is not found, return a 404 error
-        if (retrievedEndpoint == null) {
-            ErrorDTO errorDTO = new ErrorDTO("The requested endpoint does not exist. " +
-                    "Please check the URL and try again.");
-            sendErrorDTO(httpResponse, errorDTO, HttpServletResponse.SC_NOT_FOUND);
+        // If the client tries to access static resources, let the request pass through the filter chain.
+        if (requestedURL.startsWith("/assets") || requestedURL.startsWith("/webjars")) {
+            filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
 
-        // If the endpoint requires authentication and the user is not authenticated, return a 401 error
-        if (retrievedEndpoint.needsAuthentication() && !isUserAuthenticated(httpRequest)) {
-            ErrorDTO errorDTO = new ErrorDTO("You must be authenticated to access this endpoint. " +
-                    "Please log in and try again.");
-            sendErrorDTO(httpResponse, errorDTO, HttpServletResponse.SC_UNAUTHORIZED);
+        // If the client tries to access an API endpoint, let the request pass through the filter chain.
+        if (requestedURL.startsWith("/api")) {
+            filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
 
-        // If the endpoint does not require authentication and the user is authenticated, return a 403 error
-        if (!retrievedEndpoint.needsAuthentication() && isUserAuthenticated(httpRequest)) {
-            ErrorDTO errorDTO = new ErrorDTO("You are already authenticated. " +
-                    "Please log out and try again.");
-            sendErrorDTO(httpResponse, errorDTO, HttpServletResponse.SC_FORBIDDEN);
+        // If the client tries to access the login page, check the authentication status.
+        if (requestedURL.equals("/authentication.html")) {
+            if (isUserAuthenticated(httpRequest)) {
+                httpResponse.sendRedirect(httpRequest.getContextPath() + "/index.html");
+            } else {
+                filterChain.doFilter(servletRequest, servletResponse);
+            }
             return;
         }
 
-        // Continue with the filter chain
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
+        // If the client tries to access the index page, check the authentication status.
+        if (requestedURL.equals("/index.html")) {
+            if (isUserAuthenticated(httpRequest)) {
+                filterChain.doFilter(servletRequest, servletResponse);
+            } else {
+                httpResponse.sendRedirect(httpRequest.getContextPath() + "/authentication.html");
+            }
+            return;
+        }
 
-    /**
-     * Sends an error response in JSON format.
-     *
-     * @param resp       the HttpServletResponse object
-     * @param errorDTO   the ErrorDTO object containing the error message
-     * @param httpStatus the HTTP status code to be set in the response
-     * @throws IOException if an I/O error occurs
-     */
-    private void sendErrorDTO(HttpServletResponse resp, ErrorDTO errorDTO, int httpStatus) throws IOException {
-        resp.setStatus(httpStatus);
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        resp.getWriter().println(new Gson().toJson(errorDTO));
+        // If the client tries to access any other page, redirect to the index page.
+        httpResponse.sendRedirect(httpRequest.getContextPath() + "/index.html");
     }
 
     /**
